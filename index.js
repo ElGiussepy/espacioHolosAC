1c3cf76385eb0bbd26124813044baf13a4900fc9
ElGiussepy <elgiussepy@gmail.com>
2023-04-12 22:30:48 -0300

    server on

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola, mundo!');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
