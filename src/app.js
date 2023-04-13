c2ef910c731d8f239c0cd0b4ed6e0e577e3d5671
ElGiussepy <elgiussepy@gmail.com>
2023-04-13 16:51:49 -0300

    saque comentario

--- src/app.js
+++ src/app.js
@@ -4,17 +4,13 @@ import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
 const app = express();
 
-
-
-
-
 // Configuramos el motor de plantillas de express-handlebars
 app.engine('handlebars', handlebars.engine())
 app.set('view engine', 'handlebars')
 
 // Configuramos la ruta donde se encuentran las vistas
-//app.set('views', __dirname + '/views/layouts');
-app.set('views', __dirname + '/views');
+
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('main');
 });
